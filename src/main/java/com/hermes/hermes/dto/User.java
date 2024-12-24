5356261bffcba4a1742bbc0bd8c3ecbe0464d069
shkimes <shkimes09@gmail.com>
2024-12-24 17:50:33 +0900

    로그인

--- src/main/java/com/hermes/hermes/dto/User.java
+++ src/main/java/com/hermes/hermes/dto/User.java
@@ -1,9 +1,21 @@
 package com.hermes.hermes.dto;
 
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.*;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@ToString
 public class User {
-    private String id;
-    private String name;
-    private String password;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private String user_id;
+    private String user_pw;
+    private String user_name;
     private String user_verification_answer;
-    private String history;
+    private String user_history;
 }
