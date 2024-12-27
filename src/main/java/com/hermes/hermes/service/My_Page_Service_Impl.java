a62a7b95d15093c172114b0012bf761ebe3cd4ed
sin-doctor <sbs180263@gmail.com>
2024-12-27 22:31:02 +0900

    Fix circular reference error

--- src/main/java/com/hermes/hermes/service/My_Page_Service_Impl.java
+++ src/main/java/com/hermes/hermes/service/My_Page_Service_Impl.java
@@ -7,9 +7,6 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class My_Page_Service_Impl implements My_Page_Service {
-    @Autowired
-    private My_Page_Service my_Page_Service;
-
     @Override
     public User My_Page_Get_User_Info(int reg_num) {
         return UserMapper.My_Page_Get_User_Info(reg_num); // My_Page_Get_User_Info() static 에러
