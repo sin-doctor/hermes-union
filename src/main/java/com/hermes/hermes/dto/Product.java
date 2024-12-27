59478e3ad8ad0b7646be2d967d651a16a3642540
sin-doctor <sbs180263@gmail.com>
2024-12-27 21:37:45 +0900

    Merge 'lhs' branch into main

--- src/main/java/com/hermes/hermes/dto/Product.java
+++ src/main/java/com/hermes/hermes/dto/Product.java
@@ -4,20 +4,21 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import lombok.*;
+import java.sql.Blob;
 
 @NoArgsConstructor
 @AllArgsConstructor
 @Getter
 @Setter
 @ToString
-public class Product{
+public class Product {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int product_reg_num;
     private String product_name;
     private int product_price;
     private String product_description;
-    private String product_image;
+    private Blob product_image;
     private String product_category;
     private int product_size;
 }
--- src/main/java/com/hermes/hermes/dto/Purchase.java
+++ src/main/java/com/hermes/hermes/dto/Purchase.java
@@ -1,16 +1,24 @@
 package com.hermes.hermes.dto;
 
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import lombok.*;
+import java.util.Date;
 
-@NoArgsConstructor
-@AllArgsConstructor
-@Getter
-@Setter
-@ToString
+@AllArgsConstructor    // 필수 생성자
+@NoArgsConstructor     // 기본생성자
+@Setter                // setter 줄임말로 사용
+@Getter                // getter 줄임말로 사용
+@ToString             //toString 줄임말로 사용
+
 public class Purchase {
+    @Id //primary key 표기로 id는 맨 위에 작성
+    @GeneratedValue(strategy = GenerationType.IDENTITY) // auto_increment 숫자자동증가임을 설정
     private int purchase_reg_num;
     private String purchase_id;
+    private String purchase_product_size;
     private String purchase_user_id;
-    private String purchase_date;
+    private Date purchase_date;
     private String purchase_status;
 }
